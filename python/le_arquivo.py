dd85b2e0efffd4ae002270837c5163caa54d8104
kevin willian <tste@outlook.com.br>
2019-05-17 09:39:43 -0300

    linha a linha, posição do vetor, limpa '\n'

--- python/le_arquivo.py
+++ python/le_arquivo.py
@@ -1,5 +1,6 @@
 texto = open('gramatica.txt', 'r')	#abre o arquivo para leitura 'r'
-arq = texto.readlines()	#arq vira um vetor com uma linha em cada posição
-for linha in arq:	#for percorre todas as posições do vetor
-	print(linha)	#printa a linha percorrida atual
-print("\n----\n"+arq[0])	#printa através da posição do vetor
+arq = texto.readlines()			#arq vira um vetor com uma linha em cada posição
+for linha in arq:			#for percorre todas as posições do vetor
+	linha=linha.rstrip('\n')	#retira o caractere '\n' da variavel que será printada
+	print(linha)			#printa a linha percorrida atual
+print("----\n"+arq[0])			#printa através da posição do vetor
