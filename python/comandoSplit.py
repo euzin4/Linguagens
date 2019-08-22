4fd122a0df01a7528a1b6e3852ce06a1b34a1342
kevin willian <tste@outlook.com.br>
2019-08-22 14:53:09 -0300

    separa uma string usando um caractere especifico

---
+++ python/comandoSplit.py
@@ -0,0 +1,4 @@
+str = "this is string example....wow!!!"
+print (str.split( ))	#separa aonde tem espaço
+print (str.split('i',1))	#separa aonde tem o '1'º 'i'
+print (str.split('w'))	#separa aonde tem 'w'
